fab1340470a403baabfd5331bdb35170d563a8eb
sarahsoares1-byte <sarah_soares1@estudante.sesisenai.org.br>
2026-09-17 09:42:51 -0300

    adiciona exercícios 5 e 6 com cálculos de preço e verificação de paridade

---
+++ parte1-variaveis/exercicio-5.py
@@ -0,0 +1,4 @@
+produto = float(input("digite o preço do produto: "))
+quantidade = int(input("digite a quantidade comprada: "))
+total = produto * quantidade
+print(f"o valor total da compra é: R$ {total:.2f}")
