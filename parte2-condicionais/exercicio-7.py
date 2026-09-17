b9e2c205930835f8b30186522ef7cd8c633be9bb
sarahsoares1-byte <sarah_soares1@estudante.sesisenai.org.br>
2026-09-17 09:56:56 -0300

    adiciona exercícios 7 e 8 com comparações de números

---
+++ parte2-condicionais/exercicio-7.py
@@ -0,0 +1,8 @@
+numero1 = float(input("digite o primeiro numero: "))
+numero2 = float(input("digite o segundo numero: "))
+if numero1 > numero2:
+    print("o primeiro número é maior")
+elif numero2 > numero1:
+    print("o segundo número é maior")
+else:
+    print("os números são iguais")
---
+++ parte2-condicionais/exercicio-8.py
@@ -0,0 +1,7 @@
+numero = int(input("digite um número: "))
+if numero > 0:
+    print("o número é positivo")
+elif numero < 0:
+    print("o número é negativo")
+else:
+    print("o número é zero")
